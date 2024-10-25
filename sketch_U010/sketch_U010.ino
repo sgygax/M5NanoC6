0a65185e199924fed9b8cbaf8c6bf8d4dbdc481c
sgygax <stefan.gygax@gee.ch>
2024-10-25 22:46:45 +0200

    Add files via upload

---
+++ sketch_U010/sketch_U010.ino
@@ -0,0 +1,35 @@
+// Distanz-Sensor (25..1000mm) mit VL53L0x Chip
+// https://docs.m5stack.com/en/unit/TOF
+// https://github.com/adafruit/Adafruit_VL53L0X
+
+//-------------------------------------------------------------------
+#include <Adafruit_VL53L0X.h>
+
+//-------------------------------------------------------------------
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+//-------------------------------------------------------------------
+void setup(void)
+{
+  Serial.begin(115200);
+
+  if (lox.begin() == false) {
+    while (1) {
+      Serial.println("Kein Distanz-Sensor gefunden...");
+    }
+  }
+
+  lox.startRangeContinuous();
+}
+
+//-------------------------------------------------------------------
+void loop(void)
+{
+  if (lox.isRangeComplete()) {
+    Serial.print("Distanz: ");
+    Serial.print( lox.readRange() );
+    Serial.println(" mm");
+  }
+
+  //delay(100);
+}
---
+++ sketch_U057/sketch_U057.ino
@@ -0,0 +1,24 @@
+// Gabel-Lichtschranke 90°
+// https://docs.m5stack.com/en/unit/OP.90
+
+//-------------------------------------------------------------------
+const int PIN_DIGITAL_SIGNAL = 1;
+
+//-------------------------------------------------------------------
+void setup(void)
+{
+  Serial.begin(115200);
+
+  pinMode(PIN_DIGITAL_SIGNAL, INPUT);
+}
+
+//-------------------------------------------------------------------
+void loop(void)
+{
+  bool sensor_status = digitalRead(PIN_DIGITAL_SIGNAL);
+
+  Serial.print("sensor_status = ");
+  Serial.println(sensor_status);
+
+  delay(100);
+}
