6360fae9c03e6915c84f0f9e801a7d33867c305e
sgygax <stefan.gygax@gee.ch>
2024-10-29 16:28:00 +0100

    Add files via upload

---
+++ M5NanoC6_Button/M5NanoC6_Button.ino
@@ -0,0 +1,23 @@
+// Die eingebaute Taste des M5NanoC6 verwenden.
+// https://docs.m5stack.com/en/core/M5NanoC6
+
+//-------------------------------------------------------------------
+const int PIN_BUTTON = 9;
+
+//-------------------------------------------------------------------
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(PIN_BUTTON, INPUT);
+}
+
+//-------------------------------------------------------------------
+void loop()
+{
+  bool button = digitalRead(PIN_BUTTON);
+
+  Serial.println(button);
+
+  delay(100);
+}
