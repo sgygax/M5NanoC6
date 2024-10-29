756a08f8129ce54aac1037bf786cf0561f02572d
sgygax <stefan.gygax@gee.ch>
2024-10-29 16:22:46 +0100

    Add files via upload

---
+++ M5NanoC6_LED_PWM/M5NanoC6_LED_PWM.ino
@@ -0,0 +1,28 @@
+// Die eingebaute blaue LED des M5NanoC6 mit PWM ansteuern.
+// https://docs.m5stack.com/en/core/M5NanoC6
+
+//-------------------------------------------------------------------
+const int PIN_LED = 7;
+
+//-------------------------------------------------------------------
+void setup()
+{
+  // blaue led ausschalten
+  analogWrite(PIN_LED, 0);
+}
+
+//-------------------------------------------------------------------
+void loop()
+{
+  // blaue led einblenden (0..100%)
+  for (int tg = 0; tg <= 255; tg++) {
+    analogWrite(PIN_LED, tg);
+    delay(10);
+  }
+
+  // blaue led ausblenden (100..0%)
+  for (int tg = 255; tg >= 0; tg--) {
+    analogWrite(PIN_LED, tg);
+    delay(10);
+  }
+}
