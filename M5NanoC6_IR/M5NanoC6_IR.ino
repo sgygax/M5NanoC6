bc08b1db0071adf587d7a7ded897c3201bc478c8
sgygax <stefan.gygax@gee.ch>
2024-10-30 12:53:03 +0100

    Add files via upload

---
+++ M5NanoC6_IR/M5NanoC6_IR.ino
@@ -0,0 +1,23 @@
+// The built-in infrared (IR) LED.
+// https://docs.m5stack.com/en/core/M5NanoC6
+
+//-------------------------------------------------------------------
+const int PIN_LED_IR = 3;
+
+//-------------------------------------------------------------------
+void setup() 
+{
+    pinMode(PIN_LED_IR, OUTPUT);
+}
+
+//-------------------------------------------------------------------
+void loop() 
+{
+    // infrared led on
+    digitalWrite(PIN_LED_IR, HIGH);
+    delay(500);
+
+    // infrared led off
+    digitalWrite(PIN_LED_IR, LOW);
+    delay(500);
+}
