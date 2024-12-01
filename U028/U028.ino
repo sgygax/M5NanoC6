3057b4736e3b477e774f3218458f8b8efcc232df
sgygax <stefan.gygax@gee.ch>
2024-12-01 11:49:17 +0100

    Add files via upload

---
+++ U028/U028.ino
@@ -0,0 +1,38 @@
+// A infrared surface temperature measuring system using the MLX90614 chipset.
+// - https://docs.m5stack.com/en/unit/ncir
+// - https://github.com/adafruit/Adafruit-MLX90614-Library
+// - https://m5stack.oss-cn-shenzhen.aliyuncs.com/resource/docs/datasheet/hat/MLX90614-Datasheet-Melexis_en.pdf
+
+//-------------------------------------------------------------------
+#include "Adafruit_MLX90614.h"
+
+//-------------------------------------------------------------------
+Adafruit_MLX90614 sensor;
+
+//-------------------------------------------------------------------
+void setup(void)
+{
+  Serial.begin(115200);
+
+  while (sensor.begin() == false) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("Emissivity = ");
+  Serial.println(sensor.readEmissivity());
+}
+
+//-------------------------------------------------------------------
+void loop(void)
+{
+  Serial.print("Ambient = "); 
+  Serial.print(sensor.readAmbientTempC());
+  Serial.print("*C \t");
+  
+  Serial.print("Object = "); 
+  Serial.print(sensor.readObjectTempC()); 
+  Serial.println("*C");
+
+  delay(1000);
+}
