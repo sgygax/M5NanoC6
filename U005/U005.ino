81355629684ca68b5a2bf03651a9b4ad5a42032e
sgygax <stefan.gygax@gee.ch>
2024-12-03 17:25:16 +0100

    Update U005.ino

--- U005/U005.ino
+++ U005/U005.ino
@@ -1,6 +1,12 @@
 // Dreh-Potentiometer
 // - https://docs.m5stack.com/en/unit/angle
 
+// GROVE PORT (UNIVERSAL):
+// 1 GPIO1, TWI(SCL), UART(RX)
+// 2 GPIO2, TWI(SDA), UART(TX)
+// 3 +5V
+// 4 GND
+
 //-------------------------------------------------------------------
 const int PIN_ANALOG_SIGNAL = 1;
 
