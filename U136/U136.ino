a41654a3665b8ba05d0dc165a61d3280ce49b83a
sgygax <stefan.gygax@gee.ch>
2024-12-03 17:30:10 +0100

    Update U136.ino

--- U136/U136.ino
+++ U136/U136.ino
@@ -2,6 +2,12 @@
 // - https://docs.m5stack.com/en/unit/DLight%20Unit
 // - https://github.com/m5stack/M5-DLight/tree/master
 
+// GROVE PORT (UNIVERSAL):
+// 1 GPIO1, TWI(SCL), UART(RX)
+// 2 GPIO2, TWI(SDA), UART(TX)
+// 3 +5V
+// 4 GND
+
 //-------------------------------------------------------------------
 #include <M5_DLight.h>
 
