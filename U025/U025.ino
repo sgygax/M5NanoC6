bae4bbc5d330b1e1c78478ae504fff9c43535bb7
sgygax <stefan.gygax@gee.ch>
2024-12-03 17:26:36 +0100

    Update U025.ino

--- U025/U025.ino
+++ U025/U025.ino
@@ -1,6 +1,12 @@
 // 2 Tasten
 // - https://docs.m5stack.com/en/unit/dual_button
 
+// GROVE PORT (UNIVERSAL):
+// 1 GPIO1, TWI(SCL), UART(RX)
+// 2 GPIO2, TWI(SDA), UART(TX)
+// 3 +5V
+// 4 GND
+
 //-------------------------------------------------------------------
 const int PIN_BUTTON_A = 1;
 const int PIN_BUTTON_B = 2;
