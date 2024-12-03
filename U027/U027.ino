2f90dc344713574d68a84da578509a156d590694
sgygax <stefan.gygax@gee.ch>
2024-12-03 17:26:59 +0100

    Update U027.ino

--- U027/U027.ino
+++ U027/U027.ino
@@ -1,6 +1,12 @@
 // Taste
 // - https://docs.m5stack.com/en/unit/button
 
+// GROVE PORT (UNIVERSAL):
+// 1 GPIO1, TWI(SCL), UART(RX)
+// 2 GPIO2, TWI(SDA), UART(TX)
+// 3 +5V
+// 4 GND
+
 //-------------------------------------------------------------------
 const int PIN_DIGITAL_SIGNAL = 1;
 
