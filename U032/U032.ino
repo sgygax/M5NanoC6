02ae1a13a91834bd4cc401294bf18c736d8fb0f6
sgygax <stefan.gygax@gee.ch>
2024-12-03 17:28:17 +0100

    Update U032.ino

--- U032/U032.ino
+++ U032/U032.ino
@@ -2,6 +2,12 @@
 // - https://docs.m5stack.com/en/unit/gps
 // - https://github.com/Tinyu-Zhao/TinyGPSPlus-ESP32
 
+// GROVE PORT (UNIVERSAL):
+// 1 GPIO1, TWI(SCL), UART(RX)
+// 2 GPIO2, TWI(SDA), UART(TX)
+// 3 +5V
+// 4 GND
+
 //-------------------------------------------------------------------
 #include "TinyGPSPlus.h"
 
