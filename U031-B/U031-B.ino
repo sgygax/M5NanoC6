e258390115097e07a8376b1fcee56d18c89fabd7
sgygax <stefan.gygax@gee.ch>
2024-12-03 17:28:00 +0100

    Update U031-B.ino

--- U031-B/U031-B.ino
+++ U031-B/U031-B.ino
@@ -2,6 +2,12 @@
 // - https://docs.m5stack.com/en/unit/rfid2
 // - https://github.com/OSSLibraries/Arduino_MFRC522v2/tree/master
 
+// GROVE PORT (UNIVERSAL):
+// 1 GPIO1, TWI(SCL), UART(RX)
+// 2 GPIO2, TWI(SDA), UART(TX)
+// 3 +5V
+// 4 GND
+
 //-------------------------------------------------------------------
 #include <MFRC522v2.h>
 #include <MFRC522DriverI2C.h>
