5a7369217e26fc7b350d6c3fafb318beb7646184
sgygax <stefan.gygax@gee.ch>
2024-12-03 17:31:08 +0100

    Update U166.ino

--- U166/U166.ino
+++ U166/U166.ino
@@ -2,6 +2,12 @@
 // - https://docs.m5stack.com/en/unit/MiniOLED%20Unit
 // - https://github.com/olikraus/U8g2_Arduino
 
+// GROVE PORT (UNIVERSAL):
+// 1 GPIO1, TWI(SCL), UART(RX)
+// 2 GPIO2, TWI(SDA), UART(TX)
+// 3 +5V
+// 4 GND
+
 //-------------------------------------------------------------------
 #include <Wire.h>
 #include "U8g2lib.h"
