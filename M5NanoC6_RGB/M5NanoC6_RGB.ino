018c97a6795c1d703df125f97a267598b8b452d4
sgygax <stefan.gygax@gee.ch>
2024-12-03 17:19:47 +0100

    Update M5NanoC6_RGB.ino

--- M5NanoC6_RGB/M5NanoC6_RGB.ino
+++ M5NanoC6_RGB/M5NanoC6_RGB.ino
@@ -13,35 +13,40 @@ const int NUM_LEDS = 1;
 Adafruit_NeoPixel rgb(NUM_LEDS, PIN_DATA_RGB, NEO_GRB + NEO_KHZ800);
 
 //-------------------------------------------------------------------
-void setup() 
+void setup()
 {
-    pinMode(PIN_POWER_RGB_ENABLE, OUTPUT);
-    digitalWrite(PIN_POWER_RGB_ENABLE, HIGH);
+  pinMode(PIN_POWER_RGB_ENABLE, OUTPUT);
+  digitalWrite(PIN_POWER_RGB_ENABLE, HIGH);
 
-    rgb.begin();
-    rgb.show();
+  rgb.begin();
+  rgb.show();
 }
 
 //-------------------------------------------------------------------
-void loop() 
+void loop()
 {
-    // rot 100%
-    rgb.setPixelColor(0, rgb.Color(255, 0, 0));
-    rgb.show();
-    delay(500);
+  // R=100% G=0%, B=0%
+  rgb.setPixelColor(0, 255, 0, 0);
+  rgb.show();
+  delay(500);
 
-    // grün 100%
-    rgb.setPixelColor(0, rgb.Color(0, 255, 0));
-    rgb.show();
-    delay(500);
+  // R=0% G=100%, B=0%
+  rgb.setPixelColor(0, 0, 255, 0);
+  rgb.show();
+  delay(500);
 
-    // blau 100%
-    rgb.setPixelColor(0, rgb.Color(0, 0, 255));
-    rgb.show();
-    delay(500);
+  // R=0% G=0%, B=100%
+  rgb.setPixelColor(0, 0, 0, 255);
+  rgb.show();
+  delay(500);
 
-    // weiss 100%
-    rgb.setPixelColor(0, rgb.Color(255, 255, 255));
-    rgb.show();
-    delay(1000);
+  // R=100% G=100%, B=100%
+  rgb.setPixelColor(0, 255, 255, 255);
+  rgb.show();
+  delay(500);
+
+  // R=0% G=0%, B=0%
+  rgb.setPixelColor(0, 0, 0, 0);
+  rgb.show();
+  delay(500);
 }
