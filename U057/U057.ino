6925e60636df5a76312091537f21d6181b83e2ed
sgygax <stefan.gygax@gee.ch>
2024-12-03 17:28:30 +0100

    Update U057.ino

--- U057/U057.ino
+++ U057/U057.ino
@@ -1,6 +1,12 @@
 // Gabel-Lichtschranke 90°
 // - https://docs.m5stack.com/en/unit/OP.90
 
+// GROVE PORT (UNIVERSAL):
+// 1 GPIO1, TWI(SCL), UART(RX)
+// 2 GPIO2, TWI(SDA), UART(TX)
+// 3 +5V
+// 4 GND
+
 //-------------------------------------------------------------------
 const int PIN_DIGITAL_SIGNAL = 1;
 
