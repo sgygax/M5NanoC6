d78090820274a4ca973608874aea97681ebd87bd
sgygax <stefan.gygax@gee.ch>
2024-12-03 17:26:17 +0100

    Update U024-C.ino

--- U024-C/U024-C.ino
+++ U024-C/U024-C.ino
@@ -1,6 +1,12 @@
 // Joystick mit Tastenfunktion
 // - https://docs.m5stack.com/en/unit/joystick_1.1
 
+// GROVE PORT (UNIVERSAL):
+// 1 GPIO1, TWI(SCL), UART(RX)
+// 2 GPIO2, TWI(SDA), UART(TX)
+// 3 +5V
+// 4 GND
+
 //-------------------------------------------------------------------
 #include <Wire.h>
 
