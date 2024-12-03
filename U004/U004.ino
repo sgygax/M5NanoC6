ddd726c0b7a8ec9d2466ddfec972b8cf943d9e49
sgygax <stefan.gygax@gee.ch>
2024-12-03 17:24:56 +0100

    Update U004.ino

--- U004/U004.ino
+++ U004/U004.ino
@@ -1,6 +1,12 @@
 // An infrared radiation detecting system.
 // - https://docs.m5stack.com/en/unit/PIR
 
+// GROVE PORT (UNIVERSAL):
+// 1 GPIO1, TWI(SCL), UART(RX)
+// 2 GPIO2, TWI(SDA), UART(TX)
+// 3 +5V
+// 4 GND
+
 //-------------------------------------------------------------------
 const int PIN_DIGITAL_SIGNAL = 1;
 
