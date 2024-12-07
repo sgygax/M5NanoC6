6f2842690503490e64df70aebafe2a04c4b821eb
sgygax <stefan.gygax@gee.ch>
2024-12-07 17:06:55 +0100

    Add files via upload

---
+++ M5NanoC6_Neopixel50/M5NanoC6_Neopixel50.ino
@@ -0,0 +1,66 @@
+// An external Neopixel-stripe with 50 RGB-LEDs (RGB order of primary color).
+// - https://docs.m5stack.com/en/core/M5NanoC6
+
+// GROVE PORT (UNIVERSAL):
+// 1 GPIO1, TWI(SCL), UART(RX)
+// 2 GPIO2, TWI(SDA), UART(TX)
+// 3 +5V
+// 4 GND
+
+//-------------------------------------------------------------------
+#include <Adafruit_NeoPixel.h>
+
+//-------------------------------------------------------------------
+const int PIN_DATA_STRIPE = 1; // 1, 2
+
+const int NUM_LEDS = 50;
+
+// Adafruit_NeoPixel stripe(NUM_LEDS, PIN_DATA_STRIPE, NEO_GRB + NEO_KHZ800); // GRB order of primary color
+Adafruit_NeoPixel stripe(NUM_LEDS, PIN_DATA_STRIPE, NEO_RGB + NEO_KHZ800); // RGB order of primary color
+
+
+//-------------------------------------------------------------------
+void setup()
+{
+  stripe.begin();
+  stripe.show();
+}
+
+//-------------------------------------------------------------------
+void loop()
+{
+  for (int x = 0; x < NUM_LEDS; x++) {
+    // R=100% G=0%, B=0%
+    stripe.setPixelColor(x, 255, 0, 0);
+  }
+  stripe.show();
+  delay(1000);
+
+  for (int x = 0; x < NUM_LEDS; x++) {
+    // R=0% G=100%, B=0%
+    stripe.setPixelColor(x, 0, 255, 0);
+  }
+  stripe.show();
+  delay(1000);
+
+  for (int x = 0; x < NUM_LEDS; x++) {
+    // R=0% G=0%, B=100%
+    stripe.setPixelColor(x, 0, 0, 255);
+  }
+  stripe.show();
+  delay(1000);
+
+  for (int x = 0; x < NUM_LEDS; x++) {
+    // R=100% G=100%, B=100%
+    stripe.setPixelColor(x, 255, 255, 255);
+  }
+  stripe.show();
+  delay(1000);
+
+  for (int x = 0; x < NUM_LEDS; x++) {
+    // R=0% G=0%, B=0%
+    stripe.setPixelColor(x, 0, 0, 0);
+  }
+  stripe.show();
+  delay(1000);
+}
